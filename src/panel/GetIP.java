f963d28ca0646e0abfa26dabc290d6a8f27852ad
lwi-wil <nikstrach@gmail.com>
2021-06-02 10:30:06 +0300

    d

--- src/panel/GetIP.java
+++ src/panel/GetIP.java
@@ -1,14 +1,17 @@
 package panel;
 
 import java.io.IOException;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class GetIP {
 
     String IP;
     public GetIP(){
-        try {
-            InetAddress ip = InetAddress.getLocalHost();
+        try (final DatagramSocket socket = new DatagramSocket()){
+            socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
+            InetAddress ip = socket.getLocalAddress();
+            //InetAddress ip = InetAddress.getLocalHost();
             System.out.println("Current IP address : " + ip.getHostAddress());
             IP = ip.getHostAddress();
         }
